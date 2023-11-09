1848b038ad7c9fb244e3576feae4759a95d0a59e
Dhaakila <dhaakila@edu.lapinamk.fi>
2023-11-09 16:28:07 +0200

    Push 1
    Lets go

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// App.jsx
+import React, { useState } from 'react';
+import Login from './LoginPage';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = (status) => {
+    setLoggedIn(status);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      {isLoggedIn ? (
+        <div>
+          <h2>Welcome to the Main Page!</h2>
+          {/* Your main page content goes here */}
+        </div>
+      ) : (
+        <Login onLogin={handleLogin} />
+      )}
+    </div>
+  );
+};
 
-export default App
+export default App;
