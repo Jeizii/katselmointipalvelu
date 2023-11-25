01478398f7efca3ad38afec88f4850c30a52ed52
jakarvo <jakarvo@edu.lapinamk.fi>
2023-11-25 19:05:09 +0200

    Github ja Google login

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react'
 import { createClient } from '@supabase/supabase-js'
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
+import { SignInWithOAuthCredentials } from '@supabase/supabase-js'
 
 const supabase = createClient('https://ozvupwelqotiudtrymxk.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96dnVwd2VscW90aXVkdHJ5bXhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk1Mjc3NDEsImV4cCI6MjAxNTEwMzc0MX0.4mzq3iSIVo9Kd9ONOtif-vXEoHssSusBZ48D3Ndntio')
 
@@ -30,6 +31,19 @@ export default function App() {
       console.error('Error signing out:', error.message);
     }
   };
+
+  const signInWithGithub = async () => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'github',
+    })
+  }
+  async function handleSignInWithGoogle(response) {
+    const { data, error } = await supabase.auth.signInWithIdToken({
+      provider: 'google',
+      token: response.credential,
+      nonce: 'NONCE', // must be the same one as provided in data-nonce (if any)
+    })
+  }
 
 
 
