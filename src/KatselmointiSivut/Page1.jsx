521382716b3769ee5001c326d82d7466bf0aa867
Matias Hiltunen <matias.s.j@hotmail.com>
2023-12-04 16:11:37 +0200

    add examples

--- src/KatselmointiSivut/Page1.jsx
+++ src/KatselmointiSivut/Page1.jsx
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import FileUploadComponent from '../components/FileUploadComponent';
+import React, { useState } from "react";
+import styled from "styled-components";
+import FileUploadComponent from "../components/FileUploadComponent";
+import { supabase } from "../services/supabase";
 const Container = styled.div`
   display: grid;
   place-items: center;
   height: 100vh;
   font-size: 16px;
   color: #333;
-  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
   margin-bottom: 12px;
   font-weight: bold;
 `;
@@ -72,7 +73,15 @@ const StyledButton = styled.button`
   cursor: pointer;
 `;
 
-const CheckboxGroup = ({ label, options, selectedOptions, handleCheckboxChange }) => {
+const publicImageBaseURL =
+  "https://ozvupwelqotiudtrymxk.supabase.co/storage/v1/object/public/testbucket/";
+
+const CheckboxGroup = ({
+  label,
+  options,
+  selectedOptions,
+  handleCheckboxChange,
+}) => {
   return (
     <div>
       <StyledLabel>{label}:</StyledLabel>
@@ -97,6 +106,8 @@ const Page1 = () => {
   const [selectedOptionsValinta1, setSelectedOptionsValinta1] = useState(["-"]);
   const [selectedOptionsValinta2, setSelectedOptionsValinta2] = useState(["-"]);
   const [selectedOptionKampus, setSelectedOptionKampus] = useState("-");
+
+  const [filesUploaded, setFilesUploaded] = useState([]);
 
   const handleDropdownChangeTila = (event) => {
     setSelectedOptionsTila([event.target.value]);
@@ -128,12 +139,96 @@ const Page1 = () => {
     });
   };
 
+  const handleUpload = async () => {
+
+    if (filesUploaded.length > 0) {
+      const filePromises = filesUploaded.map((file) => {
+        return supabase.storage
+          .from("testbucket")
+          .upload(`/${file.name}`, file);
+      });
+
+      const files = await Promise.allSettled(filePromises);
+
+      return files.map((response) => {
+
+      
+
+        if(response.status === 'rejected'){
+          console.log(response.reason)
+          return null
+        }
+
+        const {data, error} = response.value
+
+        if (error) {
+          console.error("Error uploading file:", error.message);
+          return null
+        } else {
+          console.log("File uploaded successfully:", data);
+          return data.path;
+        }
+      });
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    
+    const images = await handleUpload();
+
+    console.log(images)
+    
+    const payload = {
+      selectedOptionsTila,
+      selectedOptionsValinta1,
+      selectedOptionKampus,
+      files: images
+    };
+    console.log(payload);
+
+    // Tässä lähetys supabasen tietokantaan
+
+    /* const { data, error } = await supabase
+      .from('Form').insert([{
+          group: "Olli Opettaja, Jaska Jokunen",
+          campus: "Rovaniemi",
+          space: "B321",
+          condition: "Hyvä",
+          notes: "Kaikki ok",
+          images: images.join(), // Tekee arraysa stringin: "kuva_abc.png,kuva_123.jpg"
+          other_notes: ""
+        },
+      ])
+      .select() */
+
+      
+      // Voidaan navigoida esim. sivulle jossa tehtyä katselmointia voidaan tarkastella 
+      // id:n perusteella
+      // navigate("/katselmoinnit/" + data.id)
+      
+      /* 
+      
+      Relaatiot.
+
+      Tässä esimerkissä image on omassa taulussa. Imagella on formin id jolle se kuuluu,
+      joten se voidaan hakea mukaan dataan yhdellä queryllä seuraavasti:
+
+      const { data, error } = await supabase.from('Form').select(`
+        *,
+        image ( path )
+      `).eq("id", 1) 
+      
+      */
+  };
+
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
       <Container>
         <FormBox>
           <h2>Jatkuva katselmointi</h2>
-          <StyledForm>
+          <StyledForm onSubmit={handleSubmit}>
             <StyledLabel>
               Katselmointiryhmä:
               <StyledInput type="text" name="text" />
@@ -149,7 +244,10 @@ const Page1 = () => {
             <p></p>
             <StyledLabel>
               Kampus:
-              <StyledSelect value={selectedOptionKampus} onChange={handleDropdownChangeKampus}>
+              <StyledSelect
+                value={selectedOptionKampus}
+                onChange={handleDropdownChangeKampus}
+              >
                 <option value="-">-</option>
                 <option value="LAY">Lapin yliopisto</option>
                 <option value="LAMK Rovaniemi">Lapin ammattikorkeakoulu</option>
@@ -160,7 +258,10 @@ const Page1 = () => {
             <p></p>
             <StyledLabel>
               Tila:
-              <StyledSelect value={selectedOptionsTila[0]} onChange={handleDropdownChangeTila}>
+              <StyledSelect
+                value={selectedOptionsTila[0]}
+                onChange={handleDropdownChangeTila}
+              >
                 <option value="-">-</option>
                 <option value="A310">A310</option>
                 <option value="A311">A311</option>
@@ -174,24 +275,28 @@ const Page1 = () => {
 
             <p></p>
             <StyledLabel>
-            <CheckboxGroup
-              label="Yleisilme"
-              options={['Hyvä', 'Tyydyttävä', 'Huono']}
-              selectedOptions={selectedOptionsValinta1}
-              handleCheckboxChange={handleCheckboxChangeValinta1}
-            />
+              <CheckboxGroup
+                label="Yleisilme"
+                options={["Hyvä", "Tyydyttävä", "Huono"]}
+                selectedOptions={selectedOptionsValinta1}
+                handleCheckboxChange={handleCheckboxChangeValinta1}
+              />
             </StyledLabel>
 
             <p></p>
             <p></p>
-           
 
             <p></p>
 
             <StyledLabel>
               Muuta huomioita / kehitysideat:
               <StyledTextArea></StyledTextArea>
-              <FileUploadComponent></FileUploadComponent>
+              <FileUploadComponent
+                setFilesUploaded={setFilesUploaded}
+              ></FileUploadComponent>
+
+              {filesUploaded.map(f => f.name).join(" ")}
+      
               <p>Mitä positiivista olet huomannut tarkastusjaksolla?</p>
               <StyledTextArea></StyledTextArea>
             </StyledLabel>
