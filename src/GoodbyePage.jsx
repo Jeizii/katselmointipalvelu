f9d1aef545f90d97d541b3b8c9370dc2766bc9c3
Dhaakila <dhaakila@edu.lapinamk.fi>
2023-12-05 15:21:46 +0200

    Hups
    xd

--- src/GoodbyePage.jsx
+++ src/GoodbyePage.jsx
@@ -2,41 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-const StyledLink = styled(Link)`
-  color: blue;
-  text-decoration: none;
-  margin: 1rem;
-  position: relative;
-`;
-
-const ExternalLink = styled.a`
-  color: blue;
-  text-decoration: none;
-  margin: 1rem;
-  position: relative;
-`;
 
 const GoodbyePage = () => {
   return (
     <body>
       <div style={{ textAlign: 'center', marginTop: '50px' }}>
-        <h1>Tämä on Lapin ammattikorkeakoulun tilojen katselmointi sovellus</h1>
-
-        {/* External Link */}
-        <ExternalLink href="https://lucit-my.sharepoint.com/:p:/g/personal/dhaakila_edu_lapinamk_fi/EaLPAHL24LdCqF3FLcd-1BMBmQjXtah6TCs7h7nbN8KYAA?e=ZVbqgv" className="btn" target="_blank" rel="noopener noreferrer">
-          LINKKI OHJEISTUKSEEN
-        </ExternalLink>
+        <h1>Moimoi kiitos katselmoinnin palauttamisesta</h1>
 <p></p>
-         {/* Internal Links */}
-         <StyledLink to="/raportti1" className="btn">
-          Raportit
-        </StyledLink>
-        <StyledLink to="/raportti2" className="btn">
-          Raportti 2
-        </StyledLink>
 
-        <h3></h3>
-        <h3></h3>
       </div>
     </body>
   );
