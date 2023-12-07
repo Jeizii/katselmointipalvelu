07d4a8d3b2e30ace92b4fcb2b3e19d04629231b3
Dhaakila <dhaakila@edu.lapinamk.fi>
2023-12-07 18:23:15 +0200

    Raporttisivujen header
    Vaihtuu nyt if lauseen avulla

--- src/RaporttiSivut/TarRaportit.jsx
+++ src/RaporttiSivut/TarRaportit.jsx
@@ -6,39 +6,42 @@ import { useEffect, useState } from "react";
 
 export async function loader({params}){
  const approved = params.approved == "approved"
+ let headerState = "";
  
   if (approved){
-  const { data, error } = await supabase.from(params.table).select(`*`).not("approvedAt", "is", null).order('created_at', {ascending: false})
+  const { data, error } = await supabase.from(params.table).select(`*`).not("approvedAt", "is", null).order('created_at', {ascending: false});
+    headerState = "Hyväksytyt raportit";
 
-  return {data,approved}
+  return {data,approved,headerState}
   }
   else{
-    const { data, error } = await supabase.from(params.table).select(`*`).is("approvedAt", "NULL").order('created_at', {ascending: false})
-
-  return {data,approved}
+    const { data, error } = await supabase.from(params.table).select(`*`).is("approvedAt", "NULL").order('created_at', {ascending: false});
+    headerState = "Tarkistettavat raportit";
+  return {data,approved, headerState}
   }
 }
 
 const TarRap = () => {
 
-const {data, approved} = useLoaderData()
-const [audits, setAudits] = useState(data)
+    const {data, approved, headerState} = useLoaderData()
+    const [audits, setAudits] = useState(data)
 
-useEffect(() => {}, [audits, setAudits])
-const handleRowClick = async ({data, index}) => {
-if (approved) return
-data.approvedAt = new Date()
-const { data:[updatedData], error } = await supabase.from('Form').upsert([data]).select()
-console.log(updatedData)
-setAudits((state) => {
-  state[index] = updatedData
+    useEffect(() => {}, [audits, setAudits])
+    const handleRowClick = async ({data, index}) => {
+    if (approved) return
+    data.approvedAt = new Date()
+    const { data:[updatedData], error } = await supabase.from('Form').upsert([data]).select()
+    console.log(updatedData)
+    setAudits((state) => {
+      state[index] = updatedData
   return [...state]
 })
 }
 
     return (
+        
         <div style={{ textAlign: 'center', marginTop: '50px',display: "flex", justifyContent: "center", flexDirection:"column" }}>
-          <h1>Tarkistettavat raportit</h1>
+          <h1>{headerState}</h1>
 
 {/* {"id":5,"created_at":"2023-12-05T12:50:46.990301+00:00","group":"Olli Opettaja, Jaska Jokunen",
 "campus":"Rovaniemi","space":"B321","condition":"Hyvä","notes":"Kaikki ok","images":"",
